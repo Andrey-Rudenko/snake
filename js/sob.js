17410395777d31e615982c304abf2a9a6a235252
Andrey-Rudenko <Zamoral+Andrey-Rudenko@users.noreply.github.com>
2020-03-18 11:39:26 +1000

    Update sob.js

--- js/sob.js
+++ js/sob.js
@@ -1,28 +1,32 @@
 //Кнопка вверх
 let blockUp = document.getElementById('up');
 blockUp.onclick = function(){
-    if (dir != 'down') {
+    if (dir != 'down'  && flag == 0) {
         dir = "up";
+        flag = 1;
     }
 }
 //Кнопка вниз
 let blockDown = document.getElementById('down');
 blockDown.onclick = function(){
-    if (dir != 'up') {
+    if (dir != 'up'  && flag == 0) {
         dir = "down";
+        flag = 1;
     }
 }
 //Кнопка вправо
 let blockRight = document.getElementById('right');
 blockRight.onclick = function(){
-    if (dir != 'left') {
+    if (dir != 'left'  && flag == 0) {
         dir = "right";
+        flag = 1;
     }
 }
 //Кнопка влево 
 let blockLeft = document.getElementById('left');
 blockLeft.onclick = function(){
-    if (dir != 'right') {
+    if (dir != 'right'  && flag == 0) {
         dir = "left";
+        flag = 1;
     }
 }
